31b4fec59b77b799a781459afca3f1e34d84c1d5
Daniel Ortega Alves <danielortegaalves@gmail.com>
2024-03-10 15:01:20 -0300

    Fix, traz todos os maiores e menores intervalor de producoes premiadas.

--- src/main/java/com/raspberryawards/services/impl/FilmeServiceImpl.java
+++ src/main/java/com/raspberryawards/services/impl/FilmeServiceImpl.java
@@ -24,28 +24,14 @@ public class FilmeServiceImpl implements FilmeService {
 	@Autowired
 	private FilmeRepository filmeRepository;
 
-	public Map<String, List<Map<String, Object>>> obtemIntervalosPremios() {
-		List<FilmeModel> filmesPorProdutor = obtemTodosFilmesPorProdutor();
-
-		// Agrupar filmes por produtor
-		Map<String, List<FilmeModel>> filmesAgrupados = agruparFilmesPorProdutor(filmesPorProdutor);
-
-		Map<String, List<Map<String, Object>>> result = new HashMap<>();
-		result.put("min", obtemProdutorComMenorIntervaloProducao(filmesAgrupados));
-		result.put("max", obtemProdutorComMaiorIntervaloProducao(filmesAgrupados));
-
-		return result;
-	}
-
 	public List<FilmeModel> obtemTodosFilmesPorProdutor() {
-		// Obtem os filmes vencedores
 		List<FilmeModel> todosFilmes = getAll().stream().filter(filme -> Boolean.TRUE.equals(filme.getVencedor()))
 				.collect(Collectors.toList());
 
-		// Expande os filmes cencedores para cada produtor que estão separados por ','
-		// ou 'and'
+		// Cria os filmes para cada produtor que estão separados por ',' ou 'and'
 		return todosFilmes.stream().flatMap(filme -> expandirFilmesPorProdutor(filme).stream())
 				.collect(Collectors.toList());
+
 	}
 
 	private List<FilmeModel> expandirFilmesPorProdutor(FilmeModel filme) {
@@ -66,77 +52,77 @@ public class FilmeServiceImpl implements FilmeService {
 		}).collect(Collectors.toList());
 	}
 
+	@SuppressWarnings("unchecked")
 	private List<Map<String, Object>> obtemProdutorComMaiorIntervaloProducao(Map<String, List<FilmeModel>> filmes) {
 		List<Map<String, Object>> result = new ArrayList<>();
 
-		Integer maiorIntervalo = null;
-		Map<String, Object> maiorIntervalolMap = null;
-
 		for (String produtor : filmes.keySet()) {
 
-			// Pega a lista de filmes do produtor
-			List<FilmeModel> filmesDoProdutor = filmes.get(produtor).stream().collect(Collectors.toList());
+			// Pega a lista de filmes do produtor, e ordena pelo ano de lançamento.
+			var filmesDoProdutor = filmes.get(produtor).stream()
+					.sorted(Comparator.comparingInt(FilmeModel::getAnoLancamento)).collect(Collectors.toList());
 
-			// Percorre os filmes pra encontrar o maior intervalo.
-			for (int i = 0; i < filmesDoProdutor.size() - 1; i++) {
-
-				int intervalo = filmesDoProdutor.get(i + 1).getAnoLancamento()
-						- filmesDoProdutor.get(i).getAnoLancamento();
-
-				if (intervalo == 0)
-					continue;
-
-				if (null == maiorIntervalo || intervalo >= maiorIntervalo) {
-					maiorIntervalo = intervalo;
-					maiorIntervalolMap = obtemMapaIntervaloProdutor(produtor, intervalo, filmesDoProdutor.get(i),
-							filmesDoProdutor.get(i + 1));
-				}
-			}
+			adicionaProdutoresComIntervaloCalculado(result, produtor, filmesDoProdutor);
 
 		}
 
-		// Adiciona o mapa de intervalo do produtor no resultado
-		if (maiorIntervalolMap != null) {
-			result.add(maiorIntervalolMap);
-		}
+		// Encontra o maior intervalo na lista
+		int maiorIntervalo = result.stream().mapToInt(entry -> (int) entry.get("interval")).max().orElse(0);
 
-		return result;
+		// Filtra os objetos que têm o maior intervalo
+		var produtorMaiorIntervalo = result.stream()
+				.filter(entry -> (int) entry.get("interval") == maiorIntervalo).collect(Collectors.toList());
+
+		produtorMaiorIntervalo
+				.sort(Comparator.comparingInt(map -> (int) ((Map<String, Object>) map).get("interval")).reversed());
+		
+		return produtorMaiorIntervalo;
 	}
-
+	
+	@SuppressWarnings("unchecked")
 	private List<Map<String, Object>> obtemProdutorComMenorIntervaloProducao(Map<String, List<FilmeModel>> filmes) {
 		List<Map<String, Object>> result = new ArrayList<>();
 
-		Integer menorIntervalo = null;
-		Map<String, Object> menorIntervalolMap = null;
-
 		for (String produtor : filmes.keySet()) {
 
-			// Pega a lista de filmes do produtor
-			List<FilmeModel> filmesDoProdutor = filmes.get(produtor).stream().collect(Collectors.toList());
+			// Pega a lista de filmes do produtor, e ordena pelo ano de lançamento.
+			var filmesDoProdutor = filmes.get(produtor).stream()
+					.sorted(Comparator.comparingInt(FilmeModel::getAnoLancamento)).collect(Collectors.toList());
 
-			// Percorre os filmes pra encontrar o maior intervalo.
-			for (int i = 0; i < filmesDoProdutor.size() - 1; i++) {
+			// Adiciona os produtores com intervalo calculado
+			adicionaProdutoresComIntervaloCalculado(result, produtor, filmesDoProdutor);
 
-				int intervalo = filmesDoProdutor.get(i + 1).getAnoLancamento()
-						- filmesDoProdutor.get(i).getAnoLancamento();
-
-				if (intervalo == 0)
-					continue;
-
-				if (null == menorIntervalo || intervalo < menorIntervalo) {
-					menorIntervalo = intervalo;
-					menorIntervalolMap = obtemMapaIntervaloProdutor(produtor, intervalo, filmesDoProdutor.get(i),
-							filmesDoProdutor.get(i + 1));
-				}
-			}
 		}
 
-		// Adiciona o mapa de intervalo do produtor no resultado
-		if (menorIntervalolMap != null) {
-			result.add(menorIntervalolMap);
-		}
+		// Encontra o menor intervalo na lista
+		int menorIntervalo = result.stream().mapToInt(entry -> (int) entry.get("interval")).min().orElse(0);
 
-		return result;
+		// Filtra os objetos que têm o menor intervalo
+		var produtorMenorIntervalo = result.stream()
+				.filter(entry -> (int) entry.get("interval") == menorIntervalo).collect(Collectors.toList());
+
+		produtorMenorIntervalo
+				.sort(Comparator.comparingInt(map -> (int) ((Map<String, Object>) map).get("interval")).reversed());
+		
+		return produtorMenorIntervalo;
+	}
+
+	private void adicionaProdutoresComIntervaloCalculado(List<Map<String, Object>> result, String produtor,
+			List<FilmeModel> filmesDoProdutor) {
+		
+		for (int i = 0; i < filmesDoProdutor.size() - 1; i++) {
+
+			int intervalo = filmesDoProdutor.get(i + 1).getAnoLancamento()
+					- filmesDoProdutor.get(i).getAnoLancamento();
+
+			if (intervalo == 0)
+				continue;
+
+			var produtorIntervalo = obtemMapaIntervaloProdutor(produtor, intervalo, filmesDoProdutor.get(i),
+					filmesDoProdutor.get(i + 1));
+			
+			result.add(produtorIntervalo);
+		}
 	}
 
 	private Map<String, Object> obtemMapaIntervaloProdutor(String produtor, int intervalo, FilmeModel primeiroPremio,
@@ -147,6 +133,19 @@ public class FilmeServiceImpl implements FilmeService {
 		intervalMap.put("previousWin", primeiroPremio.getAnoLancamento());
 		intervalMap.put("followingWin", premioSeguinte.getAnoLancamento());
 		return intervalMap;
+	}
+
+	public Map<String, List<Map<String, Object>>> obtemIntervalosPremios() {
+		List<FilmeModel> filmesPorProdutor = obtemTodosFilmesPorProdutor();
+
+		// Agrupar filmes por produtor
+		Map<String, List<FilmeModel>> filmesAgrupados = agruparFilmesPorProdutor(filmesPorProdutor);
+
+		Map<String, List<Map<String, Object>>> result = new HashMap<>();
+		result.put("min", obtemProdutorComMenorIntervaloProducao(filmesAgrupados));
+		result.put("max", obtemProdutorComMaiorIntervaloProducao(filmesAgrupados));
+
+		return result;
 	}
 
 	// O uso do TreeMap para ser ordenado pelas chaves (nomes dos produtores)
